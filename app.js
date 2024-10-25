dd3b977b29ddcf976b14cb8fd7515fce4cd9561e
Adarsh suryavanshi <adarshsuryavanshi3@gmail.com>
2024-10-25 23:28:29 +0530

    "Refactor app.js and authController.js to use catchAsync and AppError, and update error handling and routing logic"

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
 const express = require('express')
 const app = express();
 const authRouter = require('./routes/authRoute')
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const catchAsync = require('./utils/catchAsync');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controller/errorController');
 
 dotenv.config()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        message: 'wohoo! Best'
-    })
-})
+app.use('/api/v1/auth', authRouter)
 
-app.use('/api/v1/auth',authRouter)
+app.use('*', catchAsync(async (req, res, next) => {
+    throw new AppError(`cont't fing ${req.originalUrl} on this server`, 404)
+}))
 
-app.use('*',(req,res,next)=>{
-    res.status(404).json({
-        status:'fail',
-        message:'Routes not found'
-    })
-})
+app.use(globalErrorHandler)
+
 app.listen(process.env.APP_PORT, () => {
     console.log(`server is running on port ${process.env.APP_PORT}`)
 })
